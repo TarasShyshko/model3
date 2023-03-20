2e092101cfb67a96160d0d4425bc5d77b3685175
TarasShyshko <tarr13579@gmail.com>
2023-03-20 10:09:55 +0200

    update

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ $(document).ready(function (){
     $('.burger_menu').click(function (event) {
         $('.burger_menu,.header_menu').toggleClass('active');
     });
-        $('.slider').slick({
-            arrows: true,
-            adaptiveHeight: true,
-            speed:1000,
-            infinite: true,
-            initialSlide: 0,
-            autoplay: true,
-            autoplaySpeed: 1500,
-            centerMode:true,
-            slidesToShow:1,
+    $('.slider').slick({
+        arrows: true,
+        adaptiveHeight: true,
+        speed:4000,
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        centerMode:true,
+        slidesToShow:1,
 
-        });
+    });
 });

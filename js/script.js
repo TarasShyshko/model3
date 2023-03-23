0c6477f4b0e7b2ad4b882d30d6e0d1fe50b624b7
TarasShyshko <tarr13579@gmail.com>
2023-03-23 10:43:55 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function (){
     $('.burger_menu').click(function (event) {
         $('.burger_menu,.header_menu').toggleClass('active');
@@ -6,13 +7,11 @@ $(document).ready(function (){
         arrows: true,
         adaptiveHeight: true,
         speed:4000,
-        infinite: true,
-        initialSlide: 0,
         autoplay: true,
         autoplaySpeed: 1500,
         centerMode:true,
         slidesToShow:1,
-
+        fade: true
     });
 
 });

662bcf8c2ce8a45d730be64d7dcfb8e9b2ee1ee3
TarasShyshko <tarr13579@gmail.com>
2023-03-29 14:43:53 +0300

    Виправив всі помилки: 1)слайдер на всю ширину, кноопки не заліпають 2)у хедері при кліку на телефон, робить запрос на дзвінок 3)форматнув код 4)у фідбеці дав обмеження по вводженю телефону та обов'язковість полів

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-
 $(document).ready(function (){
     $('.burger_menu').click(function (event) {
         $('.burger_menu,.header_menu').toggleClass('active');
     });
     $('.slider').slick({
-        arrows: true,
         adaptiveHeight: true,
         speed:4000,
         autoplay: true,
